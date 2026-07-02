dd45685847a069e3af2d08ac4dd49bddf3433464
Tazmania0 <131428150+Tazmania0@users.noreply.github.com>
2026-07-02 20:59:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@
 # This is a shim to allow GitHub to detect the package, build is done with hatch
 # Taken from https://github.com/Textualize/rich
 
-import setuptools
+from setuptools import setup
 
-if __name__ == "__main__":
-    setuptools.setup(name="sensirion-ble2")
+setup(
+    name="sensirion-ble2",
+    version="0.1.0",
+    package_dir={"": "src"},
+    packages=["sensirion_ble2"],
+    install_requires=[
+        "bluetooth-data-tools>=0.1",
+        "bluetooth-sensor-state-data>=1.6",
+        "home-assistant-bluetooth>=1.6",
+        "sensor-state-data>=2.9",
+    ],
+)
